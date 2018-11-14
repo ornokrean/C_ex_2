3b0205bc70410dbb9742c75cb67bdc95601368df
ornokrean <or.nokrean@mail.huji.ac.il>
2018-11-14 13:19:29 +0200

    mac reg

--- main.c
+++ main.c
@@ -4,10 +4,16 @@
 
 
 /**
- * @def LINE_MAX_LEN 80
- * @brief The maximum atom line length
+ * @def LINE_MAX_LEN 100
+ * @brief The maximum line length
  */
 #define LINE_MAX_LEN 100
+
+/**
+ * @def LINE_MAX_LEN 100
+ * @brief The maximum atom line length
+ */
+#define MAX_SEQUENCES_LEN 100
 
 
 /**
@@ -16,7 +22,20 @@
  */
 const char START_OF_ROW = '>';
 
-void remove_char(const char *currLine, char to_remove)
+
+
+#define MAX(a, b) ((a)>(b)) ? a : b;
+
+void freeMemory(char  *names[MAX_SEQUENCES_LEN], char  *seqs[MAX_SEQUENCES_LEN], int index)
+{
+    for (int i = 0; i <= index; ++i)
+    {
+        free(names[i]);
+        free(seqs[i]);
+    }
+}
+
+void removeChar(const char *currLine, char to_remove)
 {
     char *pos;
     if ((pos = strchr(currLine, to_remove)) != NULL)
@@ -24,8 +43,8 @@ void remove_char(const char *currLine, char to_remove)
 }
 
 void
-create_new_sequence( FILE *file, char *names[100], char *seqs[100],  char *currLine,
-                    size_t *currLen, int *index)
+createNewSequence(FILE *file, char **names, char **seqs, char *currLine,
+                  size_t *currLen, int *index)
 {
     (*index)++;
     names[(*index)] = (char *) malloc(sizeof(char) * strlen(currLine) + 1);
@@ -36,7 +55,7 @@ create_new_sequence( FILE *file, char *names[100], char *seqs[100],  char *currL
     }
     fgets(currLine, LINE_MAX_LEN, file);
 
-    remove_char(currLine,'\n');
+    removeChar(currLine, '\n');
 
     (*currLen) = strlen(currLine);
     seqs[(*index)] = (char *) malloc(sizeof(char) * (*currLen) + 1);
@@ -44,10 +63,7 @@ create_new_sequence( FILE *file, char *names[100], char *seqs[100],  char *currL
 }
 
 
-#define MAX(a, b) ((a)>(b)) ? a : b;
-
-
-int convert_to_int(const char *to_convert)
+int convertToInt(const char *to_convert)
 {
     char *temp = NULL;
     const int res = (int) strtol(to_convert, &temp, 10);
@@ -60,7 +76,18 @@ int convert_to_int(const char *to_convert)
     return res;
 }
 
-int calc_cell(int *table, int i, int j, char *str1, char *str2, int col, int gap, int match, int
+
+size_t addToSequence(char *seqs[MAX_SEQUENCES_LEN], char *currLine, size_t currLen, int index)
+{
+    currLen += strlen(currLine);
+    seqs[index] = (char *) realloc(seqs[index], sizeof(char) * currLen + 1);
+    //FIXME handle no alloc
+    strcat(seqs[index], currLine);
+    return currLen;
+}
+
+
+int calcCell(int *table, int i, int j, char *str1, char *str2, int col, int gap, int match, int
 mismatch)
 {
     int up_val = *(table + (i - 1) * col + j) + gap;
@@ -72,7 +99,8 @@ mismatch)
     return max;
 }
 
-void initiate_table(int *table, int row, int col, int gap)
+
+void initiateTable(int *table, int row, int col, int gap)
 {
     // fill the first row
     for (int k = 0; k < col; ++k)
@@ -86,20 +114,20 @@ void initiate_table(int *table, int row, int col, int gap)
     }
 }
 
-int create_table_and_compare(char *str1, char *str2, int gap, int match, int mismatch)
+int createAndCalcMatches(char *str1, char *str2, int gap, int match, int mismatch)
 {
     int row = (int) strlen(str1) + 1, col = (int) strlen(str2) + 1;
 
     // create the table
     int *table = (int *) malloc((row) * (col) * sizeof(int));
-    initiate_table(table, row, col, gap);
+    initiateTable(table, row, col, gap);
 
     //calc all the cells
     for (int i = 1; i < row; ++i)
     {
         for (int j = 1; j < col; ++j)
         {
-            *(table + i * col + j) = calc_cell(table, i, j, str1, str2, col, gap, match, mismatch);
+            *(table + i * col + j) = calcCell(table, i, j, str1, str2, col, gap, match, mismatch);
         }
     }
     // get the match result
@@ -109,19 +137,18 @@ int create_table_and_compare(char *str1, char *str2, int gap, int match, int mis
 }
 
 
-
 int main(int argc, char *argv[])
 {
     if (argc != 5)
     {
-        fprintf(stderr,"ERROR"); //FIXME ERRROR OF NUM ARGS
+        fprintf(stderr, "ERROR"); //FIXME ERRROR OF NUM ARGS
         exit(EXIT_FAILURE);
     }
 
     char *filePath = argv[1];
-    int match = convert_to_int(argv[2]);
-    int mismatch = convert_to_int(argv[3]);
-    int gap = convert_to_int(argv[4]);
+    int match = convertToInt(argv[2]);
+    int mismatch = convertToInt(argv[3]);
+    int gap = convertToInt(argv[4]);
 
     FILE *file;
     file = fopen(filePath, "r");
@@ -131,28 +158,22 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    char *names[100];
-    char *seqs[100];
-    char currLine[102];
-    size_t currLen = 0;
+    char *names[MAX_SEQUENCES_LEN];
+    char *seqs[MAX_SEQUENCES_LEN];
+    char currLine[LINE_MAX_LEN+2];
+    size_t currLength = 0;
     int index = -1;
     while (fgets(currLine, LINE_MAX_LEN, file) != NULL)
     {
-        // remove '\n'
-        remove_char(currLine,'\n');
+        removeChar(currLine, '\n');        // remove '\n'
 
-        if (currLine[0] == START_OF_ROW) // new line
+        if (currLine[0] == START_OF_ROW) // new sequence
         {
-            create_new_sequence(file, names, seqs, currLine, &currLen, &index);
+            createNewSequence(file, names, seqs, currLine, &currLength, &index);
         }
-        else // add to current line
+        else // add to current sequence
         {
-            currLen += strlen(currLine);
-            seqs[index] = (char *) realloc(seqs[index], sizeof(char) * currLen + 1);
-            //FIXME handle no alloc
-            strcat(seqs[index], currLine);
-
-            // add the current data
+            currLength = addToSequence(seqs, currLine, currLength, index);
         }
     }
     fclose(file);
@@ -166,7 +187,7 @@ int main(int argc, char *argv[])
         {
             {
                 //create table and compare
-                int res = create_table_and_compare(seqs[i], seqs[j], gap, match, mismatch);
+                int res = createAndCalcMatches(seqs[i], seqs[j], gap, match, mismatch);
                 printf("Score for alignment of sequence %s to sequence %s is %d\n", names[i],
                        names[j], res);
 
@@ -175,14 +196,12 @@ int main(int argc, char *argv[])
     }
 
     // free all alloc
-    for (int i = 0; i <= index; ++i)
-    {
-        free(names[i]);
-        free(seqs[i]);
-
-    }
+    freeMemory(names, seqs, index);
     return 0;
 }
+
+
+
 
 
 
